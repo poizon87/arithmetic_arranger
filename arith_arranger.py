b412e3c50c3b7ae3a7f8333f47558ef19075b87b
Mike <poizon1987@gmail.com>
2022-06-08 02:00:57 -0700

    got them side by side

--- arith_arranger.py
+++ arith_arranger.py
@@ -5,7 +5,9 @@ def main():
 
 def arithmetic_arranger(args):
 
-
+    first_numbers = ''
+    second_numbers = ''
+    x = 6
     if len(args) > 5:
         print('Error: Too many problems.')
         return
@@ -31,13 +33,25 @@ def arithmetic_arranger(args):
                 print('Error: Numbers must only contain digits.')
                 return
 
-        num1 = int(prob[0])
-        op = prob[1]
-        num2 = int(prob[2])
+        num1 = str(prob[0])
+        op = str(prob[1])
+        num2 = str(prob[2])
 
-        right_align = (f"{num1: >5}\n{op} {num2: >3}")
-        print(right_align)
+        #for number in prob:
+            #dash = max(prob)
+        correct1 = f"{num1: >{x}}" + '    '
+        correct2 = op +' '+ f"{num2: >4}" + '    '
+        #right_align = (f"{num1: >6}\n{op: <} {num2: >4}\n")
+
+        #print(right_align)
+        #print(dash)
+        #print(correct)
+
+        first_numbers += correct1
+        second_numbers += correct2
+
+    print(first_numbers)
+    print(second_numbers)
 
 if __name__ == '__main__':
     main()
-
